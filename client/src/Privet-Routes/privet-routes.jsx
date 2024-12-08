fddcd75ab38d6fb0bf3bc139327900b1c7670729
uttom_kumar <uttom00781@gmail.com>
2024-12-08 23:22:31 +0600

    active website

--- client/src/Privet-Routes/privet-routes.jsx
+++ client/src/Privet-Routes/privet-routes.jsx
@@ -4,7 +4,7 @@ import {Navigate} from "react-router-dom";
 const PrivetRoutes = ({children}) => {
 
     if(!isLoggedIn()) {
-        return <Navigate to="/auth/admin/SubAdminLogin" replace />;
+        return <Navigate to="/auth/admin/login" replace />;
     }
     return children
 };
