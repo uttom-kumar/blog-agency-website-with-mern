bf548a47e33a500d12c1f1df236cd776c78e148b
uttom_kumar <uttom00781@gmail.com>
2024-12-11 00:58:39 +0600

    some problem fixed

--- client/src/component/blogs/hero-slider.jsx
+++ client/src/component/blogs/hero-slider.jsx
@@ -1,3 +1,4 @@
+import Slider from "react-slick";
 import {Link} from "react-router-dom";
 import BlogSliderSkeleton from "../../skeleton/blog-slider-skeleton.jsx";
 import SliderStore from "../../store/slider-store.js";
@@ -5,84 +6,50 @@ import SliderStore from "../../store/slider-store.js";
 
 const HeroSlider = () => {
     const{BlogSliderList} = SliderStore()
-
+    const settings = {
+        className: "",
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true
+    };
 
     return (
-        <>
-            <div className="bg-white">
-                <div id="carouselExampleDark" className="carousel hero-bg carousel-light slide">
-                    <div className="carousel-indicators">
-                        {
-                            BlogSliderList?.map((item, i) => {
-                            return (
-                                <button
-                                    key={i}
-                                    type="button"
-                                    data-bs-target="#carouselExampleDark"
-                                    data-bs-slide-to={i}
-                                    className="active bg-dark"
-                                    aria-current="true"
-                                    aria-label=""
-                                ></button>
-                            );
-                        })}
-                    </div>
-                    <div className="carousel-inner">
-                        {
-                            BlogSliderList === null? <BlogSliderSkeleton /> :
-                            BlogSliderList?.map((item, i) => {
-                            let active = "carousel-item";
-                            if (i === 0) {
-                                active = "carousel-item active";
-                            }
-                            return (
-                                <div key={i} className={active} data-bs-interval="10000">
-                                    <div className="container">
-                                        <div className="row justify-content-center">
-                                            <div className="Slider_order2 col-lg-6 col-md-12 col-sm-12 col-12  py-5">
-                                                <h3 className="headline-1 fw-bolder">
-                                                    {item?.title}
-                                                </h3>
-                                                <p className="text-dark">{item?.description}</p>
+        <div className="slider-container mx-lg-5">
+            <Slider {...settings}>
+                {
+                    BlogSliderList === null ? <BlogSliderSkeleton /> :
+                    BlogSliderList?.map((item, i) => {
+                        return (
+                            <div key={i} className="">
+                                <div className="container">
+                                    <div className="row justify-content-center">
+                                        <div className="Slider_order2 col-lg-6 col-md-12 col-sm-12 col-12  py-5">
+                                            <h3 className="headline-1 fw-bolder">
+                                                {item?.title}
+                                            </h3>
+                                            <p className="text-dark">{item?.description}</p>
 
-                                                <Link
-                                                    to={`/blogDetails/${item?._id}`}
-                                                    className="btn text-white btn-info rounded-2 mt-1 px-5"
-                                                >
-                                                    Read More
-                                                </Link>
-                                            </div>
-                                            <div className="col-lg-6 col-md-12 col-sm-12 col-12 py-5">
-                                                <img src={item["image"]} className="Slider_order1 img-fluid" alt="..."/>
-                                            </div>
+                                            <Link
+                                                to={`/blogDetails/${item?._id}`}
+                                                className="btn text-white btn-info rounded-2 mt-1"
+                                            >
+                                                Read More
+                                            </Link>
+                                        </div>
+                                        <div className="col-lg-6 col-md-12 col-sm-12 col-12 py-5">
+                                            <img src={item["image"]} className="Slider_order1 img-fluid" alt="..."/>
                                         </div>
                                     </div>
                                 </div>
-                            );
-                        })}
-                    </div>
-                    <button
-                        className="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carouselExampleDark"
-                        data-bs-slide="prev"
-                    >
-                        <span className="carousel-control-prev-icon custom-btn" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next btn"
-                        type="button"
-                        data-bs-target="#carouselExampleDark"
-                        data-bs-slide="next"
-                    >
-                        <span className="carousel-control-next-icon custom-btn" aria-hidden="true" ></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </>
-    )
-};
+                            </div>
+                        );
+                    })
+                }
+            </Slider>
+        </div>
+    );
+}
 
 export default HeroSlider;
--- client/src/pages/home-page.jsx
+++ client/src/pages/home-page.jsx
@@ -21,7 +21,7 @@ const HomePage = () => {
             <div className=" my-5">
                 <section className="container mt-5 py-5 text-center bg-white shadow">
                     <div className="mx-auto col-lg-8  ">
-                        <h2 className="mb-3">Subscribe to Our Newsletter</h2>
+                        <h2 className="mb-3">Subscribe to Our OnBlogAgency</h2>
                         <p className="text-muted mb-4">
                             Stay updated with the latest news and exclusive offers. Enter your
                             email below to subscribe!
